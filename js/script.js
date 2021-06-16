4521ecf09b519bb3ce5a502ae2907dd7e7e9a597
Dmitry <dpgomberg@gmail.com>
2021-06-16 12:07:27 +0300

    пакпвав

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ $('#sandbox-container .input-group.date').datepicker({
    language: "ru",
    orientation: "bottom auto",
    autoclose: true,
-   todayHighlight: true
+   // todayHighlight: true
 });
 console.log('fgrtgedfs');
 $(".selector").flatpickr({
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H:i",
-    time_24hr: true
+   enableTime: true,
+   noCalendar: true,
+   dateFormat: "H:i",
+   time_24hr: true
 });
-$(document).ready(function(){
-  $('#phone-number').mask('+375 (00) 000-00-00');
+$(document).ready(function () {
+   $('#phone-number').mask('+375 (00) 000-00-00');
 });
